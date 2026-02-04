144492a04d4f186b9ad902bfa4012667109225af
Mahesh Gummadi <maheshgummadi5011@gmail.com>
2026-02-04 20:17:36 +0530

    Fix URL to use vote function

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, CommentCreateView, CastVoteView, LeaderboardView
+# We import 'vote' (the function) instead of 'CastVoteView' (the old class)
+from .views import PostListView, PostDetailView, CommentCreateView, vote, LeaderboardView
 
 urlpatterns = [
     path('posts/', PostListView.as_view(), name='post-list'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:post_id>/comment/', CommentCreateView.as_view(), name='create-comment'),
-    path('vote/', CastVoteView.as_view(), name='cast-vote'),
+    path('posts/<int:post_id>/comments/', CommentCreateView.as_view(), name='create-comment'),
+    
+    # This is the line that fixed the error:
+    path('vote/', vote, name='vote'),  
+    
     path('leaderboard/', LeaderboardView.as_view(), name='leaderboard'),
 ]
